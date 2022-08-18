7c15ad64aeeadbafe9c7b03f6661099df1628331
Alcrro <alex.roventa94@gmail.com>
2022-08-18 20:59:30 +0300

    Initial Express setup

---
+++ Tutorial/serverStarter.js
@@ -0,0 +1,69 @@
+const { json } = require('express');
+const { stat } = require('fs');
+const http = require('http');
+
+const todos = [
+  {
+    id: 1,
+    text: 'todo one',
+  },
+  {
+    id: 2,
+    text: 'todo 2',
+  },
+  {
+    id: 3,
+    text: 'todo 3',
+  },
+  {
+    id: 4,
+    text: 'todo 4',
+  },
+];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  let body = [];
+  req
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+          response.error = 'Please add id and text';
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        'Content-type': 'application/json',
+        'X-Powered-By': 'Node.js',
+      });
+
+      res.end(JSON.stringify(response));
+    });
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
